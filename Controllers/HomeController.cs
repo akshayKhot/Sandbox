c5546960d1efc1e002294c574060355dd8c29729
Akshay Khot <akshay.7277@gmail.com>
2021-04-09 16:22:09 +0530

    production-ready

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Sandbox.Models;
@@ -12,14 +14,20 @@ namespace Sandbox.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+		private readonly IWebHostEnvironment environment; 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment host)
         {
             _logger = logger;
+			this.environment = host; 
         }
 
         public IActionResult Index()
         {
+			string libPath = Path.Combine(this.environment.WebRootPath, "lib");
+			string file = Directory.GetFiles(libPath).FirstOrDefault();
+			ViewBag.Script = Path.GetFileName(file);
+			
             return View();
         }
 
